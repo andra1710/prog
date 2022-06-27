e1fcf0466ecf50135f8b0e0d56a692a4bba2e61d
Kragh Bali <andra171009@gmail.com>
2022-06-27 09:58:03 +0200

    change my random apt to be a object with the integral and the number of points used in Exam

--- Exam/radapt.cs
+++ Exam/radapt.cs
@@ -1,7 +1,7 @@
 using System;
 using static System.Math;
 public class radapt{
-	public static double rad(Func<double, double> f, double a, double b, double acc, double eps, int old_N, double old_sum, double old_sum2, int N){
+	public static object[] rad(Func<double, double> f, double a, double b, double acc, double eps, int old_N, double old_sum, double old_sum2, int N){
 		/* The int N is the number of new points thrown at each iteration
 		The next variables are used when we split of our recursive*/
 		int N_left = 0;
@@ -11,23 +11,24 @@ public class radapt{
 		double sum_right = 0;
 		double sum2_right = 0;
 		// the following variables are used when we do not split up our recursive
-		var rand = new Random();
-		double width = (b-a); 
+		var rand = new Random(); // we used this to generate our random points
+		double width = b-a; // the interval we work within for x
+		double c = (a+b)/2.0; // the middle of our interval 
 		double sum = 0;
 		double sum2 = 0;
 
 		for(int i=0; i<N; i++){
-			double x = a + rand.NextDouble()*(b - a); // in a <= x <= b 
+			double x = a + rand.NextDouble()*width; // in a <= x <= b 
 			double fx = f(x);
 			sum += fx;
 			sum2 += fx*fx;
-			if (x<(a+b)/2.0){
+			if (x<c){ // if the point is in the left half of our interval
 				N_left += 1;
 				sum_left += fx;
 				sum2_left += fx*fx;
 			}
-			else{
-				N_right += 1 ;
+			else{ // if the point is in the right half of our interval
+				N_right += 1;
 				sum_right += fx;
 				sum2_right += fx*fx;
 			}
@@ -42,12 +43,22 @@ public class radapt{
 		double err = sigma*width/Sqrt(old_N); // the error
 		double tol = acc + Abs(integral)*eps; // calculating our tolerance
 		if (err < tol){ // if our error is less than the integral, we will return our integral, error and number of points
-			return integral; // make it a vector with Integral and the N??
+			object[] ResArray = new object[2];
+			ResArray[0] = integral;
+			ResArray[1] = old_N;
+			return ResArray; // make it a vector with Integral and the N??
 		}
 		else{ // making the recursive calls til we have the err < tol.
-			double Left = rad(f, a,(a+b)/2.0,acc/Sqrt(2),eps, N_left,sum_left, sum2_left, N);
-			double Right = rad(f, (a+b)/2.0, b,acc/Sqrt(2),eps,N_right,sum_right, sum2_right, N);
-			return Left+Right;
+			object[] Left = rad(f, a, c,acc/Sqrt(2),eps, N_left,sum_left, sum2_left, N);
+			object[] Right = rad(f, c, b,acc/Sqrt(2),eps,N_right,sum_right, sum2_right, N);
+			double Left_integral = Convert.ToDouble(Left[0]);
+			int Left_N = (int) Left[1];
+			double Right_integral = Convert.ToDouble(Right[0]);
+			int Right_N = (int) Right[1];
+			object[] ResArray = new object[2];
+			ResArray[0] = Left_integral+Right_integral;
+			ResArray[1] = Left_N+Right_N;
+			return ResArray;
 		}
 	} 
 }
